7099daa1a16b3c620d1f3140680a1a3ea2c941bd
Slowmoney <frolenkov58@gmail.com>
2024-09-07 16:41:01 +0300

    feat: template script

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const changeCase = require('change-case');
 import fs from 'fs';
 import path from 'path';
 import * as vscode from 'vscode';
 import { Node, readDirTree } from './read-dir-tree';
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+import { TemplateExecutor } from './template-executor';
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "file-templates" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-
 	async function showList(node: Node) {
 		const selectedCustomFile = await vscode.window.showQuickPick(node.children.map(e => e.name), { title: 'Custom Templates', });
 		if (!selectedCustomFile) {
@@ -27,42 +15,14 @@ export function activate(context: vscode.ExtensionContext) {
 	}
 
 	const createFile = async (targetPath: string, filePath: string, templateName: string) => {
-		let fileName = await vscode.window.showInputBox({ title: 'File Name' });
-		if (!fileName) { return; }
+		let originalFileName = await vscode.window.showInputBox({ title: 'File Name' });
+		if (!originalFileName) { return; }
 
-		let content = fs.readFileSync(filePath, 'utf-8');
-		let lines = content.split('\n');
+		let originalContent = fs.readFileSync(filePath, 'utf-8');
 
-		const [_, extLine] = lines.find(e => e.startsWith('#ext'))?.split(':')??[];
-		const [_1, fileNameCaseLine] = lines.find(e => e.startsWith('#filename-case'))?.split(':') ?? [];
-
-		if (fileNameCaseLine === 'snake-case') {
-			fileName = changeCase.snakeCase(fileName) + '';
-		}
-		else if (fileNameCaseLine === 'kebab-case') {
-			fileName = changeCase.kebabCase(fileName) + '';
-		}
-
-		const extIndex = lines.findIndex(e => e.startsWith('#ext'));
-		if (~extIndex) {
-			lines.splice(extIndex, 1);
-		}
-		const filenameCaseIndex = lines.findIndex(e => e.startsWith('#filename-case'));
-		if (~filenameCaseIndex) {
-			lines.splice(extIndex, 1);
-		}
-
-		content = lines.join('\n');
-		const [_2, ext] = templateName.split('.');
-
-		const resultPath = path.join(targetPath, `./${fileName}.${extLine ?? ext}`);
-		content = content.replaceAll('%filenamePascalCase', changeCase.pascalCase(fileName));
-		content = content.replaceAll('%filename', fileName);
-
-		fs.writeFileSync(resultPath, content, 'utf8');
-
-		const openPath = vscode.Uri.file(resultPath);
-		vscode.commands.executeCommand('vscode.open', openPath);
+		const executor = new TemplateExecutor({ input: originalFileName }, originalContent);
+		executor.setTargetPath(targetPath);
+		executor.exec();
 	};
 
 	const disposable = vscode.commands.registerCommand('file-templates.show', async (uri: vscode.Uri) => {
@@ -78,8 +38,8 @@ export function activate(context: vscode.ExtensionContext) {
 			name: 'root',
 			path: '',
 			children: [
+				...workspaceFolders.map(e => readDirTree(e.path, e.name)),
 				readDirTree(prefabsPath, 'examples'),
-				...workspaceFolders.map(e => readDirTree(e.path, e.name))
 			]
 		};
 
@@ -101,5 +61,4 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
---
+++ src/template-executor.ts
@@ -0,0 +1,83 @@
+import path from 'path';
+import fs from 'fs';
+const changeCase = require('change-case');
+interface FileToSave {
+    content: string[];
+    variables: Record<string, string>;
+}
+
+export class TemplateExecutor{
+    private pipes: Record<string, (input: string) => string> = {
+        ['kebab-case']: (input: string) => changeCase.kebabCase(input),
+        ['camel-case']: (input: string) => changeCase.camelCase(input),
+        ['pascal-case']: (input: string) => changeCase.pascalCase(input),
+        ['constant-case']: (input: string) => changeCase.constantCase(input),
+        ['path-case']: (input: string) => changeCase.pathCase(input),
+    };
+    targetPath: string = '';
+    constructor(private variables: Record<string, string>, private template: string) {}
+    setTargetPath(targetPath: string) {
+        this.targetPath = targetPath;
+    }
+    exec() {
+        const lines = this.template.split('\n');
+        const file: FileToSave[] = [];
+        let fileIndex = 0;
+        for (let i = 0; i < lines.length; i++) {
+            if (!file[fileIndex]) {
+                file[fileIndex] = { content: [], variables: JSON.parse(JSON.stringify(this.variables)) };
+            }
+            const line = lines[i].replaceAll('\r', '');
+            if (line.startsWith('#')) {
+                if (line.includes('#split')) {
+                    file[fileIndex].variables = JSON.parse(JSON.stringify(this.variables));
+                    fileIndex++;
+                } else {
+                    const [name, expression] = line.split(':');
+                    const varName = name.replace('#', '');
+                    this.variables[varName] = this.parseExpression(expression);
+                    file[fileIndex].variables = JSON.parse(JSON.stringify(this.variables));
+                }
+            } else {
+                file[fileIndex].content.push(this.parseLine(line));
+            }
+        }
+        for (const f of file) {
+            this.saveFile(f);
+        }
+    }
+
+    saveFile(file: FileToSave) {
+        const result = file.content.join('\n');
+        const folder = path.join(this.targetPath, file.variables['folder']);
+        fs.mkdirSync(folder, { recursive: true });
+        if (result && file.content.some(Boolean)) {
+            const resultPath = path.join(this.targetPath, file.variables['folder'], `./${file.variables['filename']}.${file.variables['ext']}`);
+            fs.writeFileSync(resultPath, result, 'utf8',);
+        }
+    }
+
+    parseLine(text: string) {
+        for (const key in this.variables) {
+            text = text.replace(`%${key}`, this.variables[key]);
+        }
+        return text;
+    }
+
+    parseExpression(expression: string) {
+        let text = this.parseLine(expression);
+
+        const [name, ...pipes] = text.split('|>');
+
+        for (const pipe of pipes) {
+            text = this.execPipe(name, pipe);
+        }
+
+        return text;
+    }
+
+    execPipe(text: string, pipeName: string): string {
+        if (!this.pipes[pipeName]) {throw new Error(`unknown pipe ${pipeName}`);}
+        return this.pipes[pipeName](text);
+    }
+}
